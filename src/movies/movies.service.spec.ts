b81798b257a881f6780c04b0765fb4f6cbd76626
hyein0112 <bhi12134@gmail.com>
2023-11-08 19:58:08 +0900

    unit testing

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -50,11 +50,53 @@ describe('MoviesService', () => {
         year: 2023,
         genres: ['호러'],
       });
-      const allMovies = service.getAll().length;
+      const beforeDelete = service.getAll().length;
       service.remove(1);
       const afterDelete = service.getAll().length;
 
-      expect(afterDelete).toBeLessThan(allMovies);
+      expect(afterDelete).toBeLessThan(beforeDelete);
+    });
+
+    it('should throw 404 error', () => {
+      try {
+        service.remove(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('create', () => {
+    it('shuld create a movie', () => {
+      const beforeCreate = service.getAll().length;
+      service.create({
+        title: 'TestMovie',
+        year: 2023,
+        genres: ['호러'],
+      });
+      const afterCreate = service.getAll().length;
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a movie', () => {
+      service.create({
+        title: 'TestMovie',
+        year: 2023,
+        genres: ['호러'],
+      });
+      service.update(1, { title: 'Updated Test' });
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('Updated Test');
+    });
+
+    it('should throw 404 error', () => {
+      try {
+        service.update(999, {});
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
     });
   });
 });
